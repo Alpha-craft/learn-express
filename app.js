0b04a3a5b047ae5f56106c00f1c078b2f9971ea1
Alpha-craft <ainurahman03@gmail.com>
2022-03-21 11:20:15 +0700

    learn respon send file

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/about',(req,res) => {
+  res.send('ini adalah halaman about');
+})
+
+app.get('/post',(req,res) => {
+  res.send("ini adalah halaman post")
+})
+
+app.get('/home',(req,res)=>{
+  res.sendFile('./index.html',{root: __dirname})
+})
+
+app.get('/api',(req,res)=>{
+  res.json({
+    nama: 'Ainurahman',
+    kelas: 'XII-SIJA'
+  });
+})
+
+// anggap saja error handler
+app.use('/',(req,res) => {
+  res.status(404)
+  res.send("Halaman tidak ada")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+  // console.log(app);
+}) 
